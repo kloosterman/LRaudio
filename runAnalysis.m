e772c21554e9f76cab3ef81f8b562f88e5aaf16c
Niels Kloosterman <kloosterman@mpib-berlin.mpg.de>
2024-01-20 10:04:43 +0100

    Update runAnalysis.m

--- runAnalysis.m
+++ runAnalysis.m
@@ -12,7 +12,7 @@ addpath(fullfile(toolspath, 'mMSE'));
 addpath(fullfile(toolspath, 'LRaudio'))
 addpath(fullfile(toolspath, 'qsub-tardis')) 
 
-outputpath = fullfile(fileparts(datapath, 'outputdata'));
+outputpath = fullfile(fileparts(datapath), 'outputdata');
 mkdir(outputpath)
 
 %% define subjects
