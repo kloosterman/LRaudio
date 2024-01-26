c8a566ef84af43182dd7f49d02475198c368dfa1
Niels Kloosterman <kloosterman@mpib-berlin.mpg.de>
2024-01-26 20:52:14 +0100

    Update computemMSE.m

--- computemMSE.m
+++ computemMSE.m
@@ -137,7 +137,7 @@ switch analysis
     cfg.channel      = 'EEG';
     cfg.method       = 'mtmconvol';
     cfg.taper        = 'hanning';
-    cfg.foi          = 2:2:30;                         % analysis 2 to 30 Hz in steps of 2 Hz
+    cfg.foi          = 2:1:30;                         % analysis 2 to 30 Hz in steps of 2 Hz
     cfg.t_ftimwin    = ones(length(cfg.foi),1).*0.5;   % length of time window = 0.5 sec
     cfg.toi          = -0.5:0.05:1.5;                  % time window "slides" from -0.5 to 1.5 sec in steps of 0.05 sec (50 ms)
     freq{1} = ft_freqanalysis(cfg, data);
